4b0940fa59eb32e2ccdc9cead1db46b761b9dda7
Caoshuai <caoshuai@yunphant.com>
2024-12-20 20:04:39 +0800

    fix: add test

--- test.go
+++ test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/go-kit/kit/endpoint"
+	"testing"
 )
 
 func MakeConcatEndpointTest(svc Service) endpoint.Endpoint {
@@ -11,4 +12,26 @@ func MakeConcatEndpointTest(svc Service) endpoint.Endpoint {
 		v := svc.Concat(req.A, req.B)
 		return concatResponse{V: v}, nil
 	}
+}
+
+type concatService struct {
+}
+
+func (c concatService) Concat(s string, s2 string) string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func TestConcatEndpoint(t *testing.T) {
+	svc := &concatService{}
+	ep := MakeConcatEndpointTest(svc)
+
+	request := concatRequest{A: string(10), B: string(20)}
+	_, err := ep(context.Background(), request)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("concat endpoint test passed")
+	t.Log(svc.Concat("hello", "world"))
 }
