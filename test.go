b367c533fc699e6433fe2b96f5d474ab5fe0c9e4
Caoshuai <caoshuai@yunphant.com>
2024-12-20 20:02:23 +0800

    fix: add test

--- test.go
+++ test.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"context"
+	"github.com/go-kit/kit/endpoint"
+)
+
+func MakeConcatEndpointTest(svc Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(concatRequest)
+		v := svc.Concat(req.A, req.B)
+		return concatResponse{V: v}, nil
+	}
+}
